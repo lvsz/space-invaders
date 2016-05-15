a34f54c6981c919b3436c377f2c9a760ab4ca71e
lvsz <e@levi.si>
2016-05-15 22:19:26 +0200

    added documentation

--- src/bunkers.rkt
+++ src/bunkers.rkt
@@ -5,19 +5,30 @@
 
 (provide bunkers-adt)
 
+;; dimensions of the bitmap below
 (define bitmap-width  22)
 (define bitmap-height 16)
 
+;; dimensions that work for the window
 (define bunker-width  (* bitmap-width  unit-width))
 (define bunker-height (* bitmap-height unit-height))
+
+;; spacing between edge of screen and first bunker
 (define margin  (* unit-width 32))
+;; spacing between bunkers
 (define spacing (* unit-width 24))
+;; height on screen of bunkers
 (define bunker-y (* unit-width 192))
+;; number of bunkers
 (define number-of-bunkers 4)
 
+;; converts an x coordinate of the window
+;; to a corresponding location in the bitmap
 (define (x->bunker-x x offset)
   (/ (- x offset) unit-width))
 
+;; the basic bunker bitmap
+;; gets modified during game-play
 (define bunker-bitmap 
   (let ((· #f)
         (█ #t))
@@ -39,11 +50,18 @@
       █ █ █ █ █ · · · · · · · · · · · · █ █ █ █ █
       █ █ █ █ █ · · · · · · · · · · · · █ █ █ █ █)))
 
+
+;;; single bunker ADT
+;;; for local use only
 (define (bunker-adt make-id x y)
   (let*
-    ((bitmap (vector-copy bunker-bitmap))
+    (;; several bunkers needed, so make copies
+     (bitmap (vector-copy bunker-bitmap))
      (id (make-id bitmap bitmap-width bitmap-height))
-     (updated? #f)
+     ;; when #f, needs updating
+     (updated #f)
+
+     ;; check if a certain bit exists and is #t
      (bit?
        (lambda (x y)
          (and (>= x 0)
@@ -51,38 +69,48 @@
               (< x bitmap-width)
               (< y bitmap-height)
               (vector-ref bitmap (+ x (* bitmap-width y))))))
+
+     ;; colission detection
      (shot!
        (lambda (x direction)
          (let-values (((start end next y)
                        (if (eq? direction 'up)
                          (values (- bitmap-height 1) -1 -1 #f)
                          (values 0 bitmap-height 1 #f))))
-         (if (let loop ((i start))
-               (cond ((= i end) #f)
-                     ((bit? x i) (set! y i))
-                     (else (loop (+ i next)))))
-           (begin
-             (vector-set! bitmap (+ x (* bitmap-width y)) #f)
-             (when (bit? x (+ y next))
-               (vector-set! bitmap (+ x (* bitmap-width (+ y next))) #f))
-             (let loop ((i (- x 3)) (j (- y 3)))
-               (when (and (bit? i j)
-                          (zero? (random (+ 1 (abs (- i x)) (abs (- j y))))))
-                 (vector-set! bitmap (+ i (* bitmap-width j)) #f))
-               (cond ((= j (+ y 2))
-                      (set! updated? #f))
-                     ((= i (+ x 2))
-                      (loop (- x 3) (+ j 1)))
-                     (else
-                       (loop (+ i 1) j)))))
-           #f))))
+           ;; first see which direction the bullet travels
+           ;; then set the y coordinate of the right bit to damage
+           ;; returns #f is bullet travels through
+           (if (let loop ((i start))
+                 (cond ((= i end) #f)
+                       ((bit? x i) (set! y i))
+                       (else (loop (+ i next)))))
+             (begin
+               (vector-set! bitmap (+ x (* bitmap-width y)) #f)
+               (when (bit? x (+ y next))
+                 (vector-set! bitmap (+ x (* bitmap-width (+ y next))) #f))
+               (let loop ((i (- x 3)) (j (- y 3)))
+                 ;; some randomness for collateral damage
+                 (when (and (bit? i j)
+                            (zero? (random (+ 1 (abs (- i x)) (abs (- j y))))))
+                   (vector-set! bitmap (+ i (* bitmap-width j)) #f))
+                 (cond ((= j (+ y 2))
+                        (set! updated #f))
+                       ((= i (+ x 2))
+                        (loop (- x 3) (+ j 1)))
+                       (else
+                         (loop (+ i 1) j)))))
+             #f))))
+
+     ;; draws the bunker only if it needs updating
      (draw!
        (lambda (window)
-         (when (not updated?)
+         (when (not updated)
+           ;; has to generate a new id
            ((window 'remove!) id)
            (set! id (make-id bitmap bitmap-width bitmap-height))
-           (set! updated? #t)
+           (set! updated #t)
            ((window 'draw!) id x y))))
+
      (dispatch
        (lambda (msg)
          (case msg
@@ -90,29 +118,41 @@
            ((draw!) draw!)))))
     dispatch))
 
+
+;;; ADT for all bunkers, gets provided to main.rkt
 (define (bunkers-adt make-id)
   (let*
-    ((bunker-list
+    (;; build the bunkers
+     (bunker-list
        (build-list number-of-bunkers
                    (lambda (i)
                      (bunker-adt make-id
                                  (+ margin (* i (+ bunker-width spacing)))
                                  bunker-y))))
+
+     ;; gets coordinates and sees if there might be a hit
      (shot!
        (lambda (x y direction)
+         ;; most impartantly, the bullet can't be higher or lower than the bunkers
          (if (or (<  y bunker-y)
                  (>= y (+ bunker-y bunker-height)))
            #f
            (let loop ((bunkers bunker-list) (offset margin))
-             (cond ((null? bunkers) #f)
-                   ((and (>= x offset)
-                         (<  x (+ offset bunker-width)))
-                    ;(displayln (cons (x->bunker-x x offset) (y->bunker-y y))))
-                    (((car bunkers) 'shot!) (x->bunker-x x offset) direction))
-                   (else (loop (cdr bunkers) (+ offset bunker-width spacing))))))))
+             (cond
+               ; no bunkers hit
+               ((null? bunkers) #f)
+               ; found a target, might still return #f if too damaged
+               ((and (>= x offset)
+                     (<  x (+ offset bunker-width)))
+                (((car bunkers) 'shot!) (x->bunker-x x offset) direction))
+               ; else continue looking
+               (else (loop (cdr bunkers) (+ offset bunker-width spacing))))))))
+
+     ;; draws all available bunkers
      (draw!
        (lambda (window)
          (for-each (lambda (bunker) ((bunker 'draw!) window)) bunker-list)))
+
      (dispatch
        (lambda (msg)
          (case msg
